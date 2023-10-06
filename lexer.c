a3096834e7a7e908880db1308a0d0317d03cc6e2
sconeyy <jcgauvin@nic.edu>
2023-10-05 17:09:31 -0700

    Lexer is complete with 2 issues, details in comments

--- lexer.c
+++ lexer.c
@@ -41,7 +41,15 @@ static Token createToken(TokenType type, const char *commentText)
     Token token;
     token.type = type;
     token.lexeme = strdup(buffer);
-    token.commentText = strdup(commentText);
+    // Print comment text
+    if (commentText == NULL)
+    {
+        token.commentText = "[NULL]";
+    }
+    else
+    {
+        token.commentText = strdup(commentText);
+    }
     memset(buffer, 0, sizeof(buffer));
     bufferIndex = 0;
     return token;
@@ -58,6 +66,17 @@ Token getNextToken()
         currentChar = fgetc(inputFile);
     }
 
+    // Checks for 'EOF' at the beginning of the function (before reading current char),
+    // since the program was reading the null terminator as an unknown lexeme.
+    if (currentChar == EOF)
+    {
+        token.type = UNK;
+        return token;
+    }
+
+    // I have been having trouble displaying the comment text specifically for hours upon hours.
+    // I'm able to detect if a comment is single or multi-line, but unable to display the actual text.
+    // I want 'some' points for this lexer so I'm going to submit with that logic unincluded.
     if (currentChar == '/')
     {
         // Check for comments
@@ -120,13 +139,17 @@ Token getNextToken()
         while (currentChar != '"' && currentChar != EOF)
         {
             addToBuffer(currentChar);
-            currentChar = fgetc(inputFile);
+            currentChar = fgetc(inputFile); // These two lines of code are repeated multiple times throughout reading of the file.
+                                            // All I'm doing is writing currentChar into the buffer and 're'writing currentChar
+                                            // as the next readable character so I have two instances at the same time.
         }
         if (currentChar == '"')
         {
             addToBuffer(currentChar);
             currentChar = fgetc(inputFile);
-            token = createToken(STRING, NULL);
+            token = createToken(STRING, NULL); // I've passed NULL into commentText for each token creation to satisfy the program.
+                                               // It doesn't do anything as far as I know, and I've fixed the logic to prevent a
+                                               // segmentation fault starting on line 45.
         }
         else
         {
@@ -171,7 +194,7 @@ Token getNextToken()
         }
         token = createToken(NUMERIC_LITERAL, NULL);
     }
-    else if (strchr(".()<>=+-*/|&;,:[],", currentChar) != NULL)
+    else if (strchr(".()<>=+-*/|&;,:[],", currentChar) != NULL) // Returning the first occurrence of a character in the string read.
     {
         // Operator
         addToBuffer(currentChar);
@@ -207,7 +230,7 @@ Token getNextToken()
     }
     else
     {
-        // Unknown token
+        // Unknown token if it fits no other categories
         addToBuffer(currentChar);
         currentChar = fgetc(inputFile);
         token = createToken(UNK, NULL);
